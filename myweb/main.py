96ad89f616ddfb14047b7d57c175ea92faf0b21f
yassinOrlando <yassinovp@gmail.com>
2020-10-10 23:26:35 -0500

    info 19

--- myweb/main.py
+++ myweb/main.py
@@ -16,7 +16,7 @@ ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 db = SQLAlchemy(app)
 
-from myweb.controller import *
+from controller import *
 
 if __name__ == '__main__':
     app.run()
