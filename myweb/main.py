b5b7f39fb078103542a46c6843c7228eb266eb00
yassinOrlando <yassinovp@gmail.com>
2020-10-11 00:17:41 -0500

    info 29

--- myweb/main.py
+++ myweb/main.py
@@ -20,6 +20,4 @@ db = SQLAlchemy(app)
 from myweb.controller import *
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host='0.0.0.0', port=port)
-    #app.run()
+    app.run()
