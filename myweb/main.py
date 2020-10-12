71e946cd6f80262276a3d6aae6df7ad5bcdfa9c0
yassinOrlando <yassinovp@gmail.com>
2020-10-11 23:45:16 -0500

    yes2

--- myweb/main.py
+++ myweb/main.py
@@ -17,7 +17,7 @@ ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
 db = SQLAlchemy(app)
 
 
-from controller import *
+from myewb.controller import *
 
 if __name__ == '__main__':
     #port = int(os.environ.get("PORT", 8000))
