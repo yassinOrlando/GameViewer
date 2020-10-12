dbe1fa79161698661ed6b61e7580092016f3fac2
yassinOrlando <yassinovp@gmail.com>
2020-10-11 19:55:17 -0500

    info 37

--- myweb/main.py
+++ myweb/main.py
@@ -22,4 +22,4 @@ from myweb.controller import *
 if __name__ == '__main__':
     #port = int(os.environ.get("PORT", 8000))
     #app.run(host='0.0.0.0', port=port)
-    app.run()
+    app.run(debug=True)
